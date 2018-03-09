423a515682f07fc975ce8c3e81e7049f7a3e9c4e
Erik Jon-And <erikjonand@gmail.com>
2018-03-09 09:33:54 +0100

    Add project files.

---
+++ LMS2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LMS2.MvcApplication" Language="C#" %>
---
+++ LMS2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMS2.Startup))]
+namespace LMS2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
