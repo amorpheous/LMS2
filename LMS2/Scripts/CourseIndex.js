81d53dc0a740ba9e72ed8f2c8f789e8fe8913fd7
Jascaar <34939709+Jascaar@users.noreply.github.com>
2018-03-16 15:07:31 +0100

    Jquery show/hide course page

---
+++ LMS2/Scripts/CourseIndex.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $('.module').addClass('hidden');
+    $('.activity').addClass('hidden');
+    });
+
+$(document).ready(function () {
+    $('.course').on('mouseenter', function () {
+        $(this).addClass('highlight');
+//        $(this).find('.photos').show();
+    });
+});
+
+$(document).ready(function () {
+    $('.course').on('mouseleave', function () {
+        $(this).removeClass('highlight');
+    });
+});
+
+
+$(document).ready(function () {
+var message = $('<span class="highlight">THIS IS JUST A TEST LINE</span>');
+$('.usa').append(message);
+});
+
+$(document).ready(function () {
+    $('.course').click(function () {
+        $(this).nextUntil('.course','.module').toggleClass("hidden");
+    });
+});
+
+$(document).ready(function () {
+    $('.module').click(function () {
+        $(this).nextUntil('.module', '.activity').toggleClass("hidden");
+    });
+});
